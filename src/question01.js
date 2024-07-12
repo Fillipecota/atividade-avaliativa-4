700fff8cfdd3e395d778a708875e8d44979a0a50
FILLIPE SANT'ANNA COTA <filipe_s_cota@estudante.sc.senai.br>
2024-07-11 22:05:40 -0300

    provar

--- src/question01.js
+++ src/question01.js
@@ -5,7 +5,7 @@ var palavra = leia.question("DIGITE UMA PALAVRA: ")
 var num = 0
 
 for(var i = 0; i < palavra.length; i++){
-    if (palavra[i] === "a"|| "e"|| "i "|| "o" || "u"){
+    if (palavra[i] === "a"|| palavra[i] === "e"|| palavra[i] === "i "|| palavra[i] === "o" || palavra[i] ==="u"){
         num ++
     }
     console.log(num)
